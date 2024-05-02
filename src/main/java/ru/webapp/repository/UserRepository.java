ce4f97b66d694291e10dcc8c32ec3774338a97e8
il <>
2024-05-03 00:46:33 +0300

    adding annotation to user

--- src/main/java/ru/webapp/repository/UserRepository.java
+++ src/main/java/ru/webapp/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package ru.webapp.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.webapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
 }
